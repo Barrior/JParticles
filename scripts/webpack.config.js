683e1585a3e06fb24b9f93ffc35d3f1fe7c8e7c8
Barrior <barrior@qq.com>
2024-03-21 13:52:47 +0800

    chore: ignore typescript errors when compile by webpack

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -53,6 +53,7 @@ module.exports = {
             loader: 'ts-loader',
             options: {
               onlyCompileBundledFiles: true,
+              transpileOnly: true,
             },
           },
         ],
